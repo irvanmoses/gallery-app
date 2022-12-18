496dc13d358acd9c34a799292fe11d4710a49ead
irvanmoses <irvanmoses10@gmail.com>
2022-12-18 09:31:18 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Irvan Moses"; // TODO: replace this
+const studentId = "FE2785853"; // TODO: replace this
+
+export { url, studentName, studentId };
