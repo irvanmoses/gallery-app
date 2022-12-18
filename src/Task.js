21b416d72082982bd28162539e3a2b50e3e4cf4f
irvanmoses <irvanmoses10@gmail.com>
2022-12-18 09:56:15 +0700

    update v4

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
+const url = "https://gallery-app-ff865.web.app/"; // TODO: replace this
 const studentName = "Irvan Moses"; // TODO: replace this
 const studentId = "FE2785853"; // TODO: replace this
 
